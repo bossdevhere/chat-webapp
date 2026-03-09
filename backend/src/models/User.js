ccc941b0abc6165e94ecd94ae89cd0061acd2307
Deven Rajput <228r1a6621@gmail.com>
2026-03-09 16:58:11 +0530

    Updated project files

---
+++ backend/src/models/User.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+    type: String,
+    required: true,
+  },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+    password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  nativeLanguage: {
+    type: String,
+    default: "",
+  },
+  learningLanguage: {
+    type: String,
+    default: "",
+  },
+  location: {
+    type: String,
+    default: "",
+  },
+  isOnBoarded: {
+    type: Boolean,
+    default: false,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+//pre hook
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    try {
+        const user = await bcrypt.hash(this.password, 10);
+        this.password = user;
+        next();
+    } catch (error) { 
+        next(error);
+    }
+});
+
+
+export default User;
